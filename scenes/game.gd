350f30f82e02c8cbe2e052451b6fd7c4c3d04d6d
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2025-09-16 18:01:55 -0300

    next fase

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var label=$Label
+var next = "res://scenes/fase2.tscn"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
