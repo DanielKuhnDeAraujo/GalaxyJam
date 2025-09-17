6babd38764c5e823b7a451de37565fae36aa1904
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2025-09-17 19:57:42 -0300

    umas coisas sla

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,12 +9,16 @@ var gravity =980
 var dash=1
 var dashw=1
 var dashdir=1
+var chao="s"
 @onready var animated_sprite = $AnimatedSprite2D
 var personagem = "sol"
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		chao="n"
+	else :
+		chao="s"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
