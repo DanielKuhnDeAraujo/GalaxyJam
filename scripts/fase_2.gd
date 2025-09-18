4c2ad9d98050b4621c5182c2fa79aee736d1bb7d
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2025-09-18 20:49:08 -0300

    Coisa pra caralho

---
+++ scripts/fase_2.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var transforma = 0
+var next ="res://scenes/fase3.tscn"
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
